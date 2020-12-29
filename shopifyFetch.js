3b8c2a348d0fa5da35615f7f7643cc1748f65738
Randy <randy@Kaboom.local>
2020-12-28 17:50:12 -0800

    Initial commit

---
+++ shopifyFetch.js
@@ -0,0 +1,89 @@
+function onOpen() {
+  SpreadsheetApp.getUi()
+    .createMenu('Ultimate Coffee')
+    .addItem('Update from Shopify', 'updateFromShopify')
+    .addToUi();
+};
+
+function updateFromShopify() {
+  updateCustomers();
+}
+
+function updateCustomers() {
+  // load the linked spreadsheet object
+  var spreadsheet = SpreadsheetApp.getActiveSpreadsheet();
+  var customersSheet = spreadsheet.getSheetByName('Customers');
+
+  var results =  shopifyApiPost(
+    '/admin/api/2020-10/customers.json?fields=id,created_at,updated_at,first_name,last_name,total_spent,orders_count,last_order_name,addresses', null);
+  var rows = [ ];
+
+  if(results.hasOwnProperty('customers') && results.customers.length) {
+    results.customers.forEach(function (customer, index) {
+      var customerInfo = [ ];
+
+      // skip customers that have zero orders
+      if(!customer.hasOwnProperty('orders_count') || !customer.orders_count) { return; }
+
+      customerInfo.push(getProperty(customer,['id']));
+      customerInfo.push(getProperty(customer,['first_name'])? getProperty(customer,['first_name'])[0]:null);
+      customerInfo.push(getProperty(customer,['last_name']));
+      customerInfo.push(new Date(getProperty(customer,['created_at'])));
+      customerInfo.push(new Date(getProperty(customer,['updated_at'])));
+      customerInfo.push(getProperty(customer,['total_spent']));
+      customerInfo.push(getProperty(customer,['orders_count']));
+      customerInfo.push(getProperty(customer,['last_order_name']));
+      
+      var zips = '';
+      if(customer.hasOwnProperty('addresses') && customer.addresses.length) {
+        customer.addresses.forEach(function (a) {
+          if(a.hasOwnProperty('zip') && a.zip) {
+            zips += (zips.length? ' ':'') + a.zip
+          }
+        });
+      }
+      
+      customerInfo.push('\''+zips);
+      rows.push(customerInfo);
+    });
+
+    customersSheet.getRange(2, 1, rows.length, rows[0].length).setValues(rows);
+  }
+}
+
+function shopifyApiPost(apiCall, postData) {
+  var request = 'https://ultimate-coffee-program.myshopify.com' + apiCall;
+  var options = {
+    headers: { "Authorization": "Basic " + Utilities.base64Encode(PropertiesService.getScriptProperties().getProperty('SHOPIFY')) },
+    contentType: 'application/json',
+  };
+
+  if(postData) {
+    options.method = 'post';
+    options.payload = JSON.stringify(postData);
+  }
+  else {
+    options.method = 'get';
+  }
+
+  var response = UrlFetchApp.fetch(request, options);
+
+  if (response.getResponseCode() === 200) {
+    try { return JSON.parse(response.getContentText()); }
+    catch(e) { }
+  }
+
+  return null;
+}
+
+// getProperty(object, property array): get value from array of nested property names (null if not present)
+function getProperty(o, p) {
+  var c = JSON.parse(JSON.stringify(o));
+  for(var i = 0; i < p.length; i++) {
+    if(!c.hasOwnProperty(p[i])) {
+      return null;
+    }
+    c = c[p[i]];
+  }
+  return c;
+}
